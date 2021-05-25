bee8b7cdf3aef26d7693cc7c256cc8ed0637bb11
Guilherme Mello <guilhermecfmello@gmail.com>
2021-05-25 19:39:26 -0300

    using golang workspace

---
+++ package/reuse/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/guilhermecfmello/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Rect(10, 15))
+	// fmt.Println(area._TrianguloEq(5.0, 2.0)) private function
+}
